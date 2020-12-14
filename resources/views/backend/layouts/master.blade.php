01691a3aff563a228ff90cd68a8f5fbeb65c22ba
moshiur rahman <moshiurcse888@gmai.com>
2020-12-14 21:29:02 +0600

    sidebar error fixed

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -109,6 +109,7 @@
     </a>
 
     <!-- Sidebar -->
+    <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
